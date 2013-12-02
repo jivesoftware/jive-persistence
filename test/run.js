40accbb855cf84e0506e6ce1e0e48d32fdd95d3e
Aron Racho <aron.racho@jivesoftware.com>
2013-12-01 17:18:08 -0800

    work

--- test/run.js
+++ test/run.js
@@ -3,23 +3,18 @@ var jive = require('jive-sdk');
 var jiveMongo = require('../');
 
 var makeRunner = function() {
-    var runner = Object.create(require('jive-testing-framework/baseSuite'));
-
-    runner.getParentSuiteName = function() {
-        return 'jive';
-    };
-
-    runner.onTestStart = function(test) {
-        test['ctx']['persistence'] = new jiveMongo({
-            'databaseUrl' : 'mongodb://localhost:27017/mongoTestDB'
-        });;
-    };
-
-    runner.onTestEnd = function(test) {
-        test['ctx']['persistence'].destroy();
-    };
-
-    return runner;
+    return testUtils.makeRunner( {
+        'eventHandlers' : {
+            'onTestStart' : function(test) {
+                test['ctx']['persistence'] = new jiveMongo({
+                    'databaseUrl' : 'mongoTestDB'
+                });
+            },
+            'onTestEnd' : function(test) {
+                test['ctx']['persistence'].destroy();
+            }
+        }
+    });
 };
 
 makeRunner().runTests(
@@ -29,6 +24,7 @@ makeRunner().runTests(
             'jive' : jive,
             'jiveMongo' : jiveMongo
         },
+        'rootSuiteName' : 'jive',
         'runMode' : 'test',
         'testcases' : process.cwd()  + '/library',
         'timeout' : 5000
